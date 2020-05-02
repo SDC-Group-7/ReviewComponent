5ecdf913187073e79258f14cb0a31c9f5b47f4c6
Shoji <stakashima21@gmail.com>
2020-05-01 18:02:05 -0700

    feat(db seeder): Add promises to seeding script

--- db/seeder.js
+++ db/seeder.js
@@ -1,41 +1,38 @@
 var mysql = require('mysql');
 var data = require('./legos_data.js');
 var helpers = require('../db/helpers.js');
-var fs = require('fs');
-var Promise = require('bluebird');
+var db= require('./index.js');
 
-var connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB
-});
-
-connection.connect();
+/*
+data
+------
+names: 100 entries - {name, age}
+products: 5 entries - {name}
+experiences: 5 entries - {play_experience: 4, difficulty: 4, value: 5, build_time: 5}
+reviews: 5 entries - {rating: 5, recommended: 1, subject: 'lorum ipsum 1', is_helpful: 5, is_not_helpful: 1, product_id: 1, experience_id: 1, user_id: 1},
+images: 5 entries - {url: 'https://aws.s3/1', review_id: 1}
+*/
 
 // Insert 100 users into the database
-const addUsers = async () => {
-  await Promise.map(data.users, (user) => {
-    return connection.query('INSERT INTO users (name, age) VALUES (?, ?)', [user.name, user.age]);
-  });
-};
-
-const addProducts = async () => {
-  await Promise.map(data.products, (product) => {
-    return connection.query('INSERT INTO products (name) VALUES (?)', [product.name]);
+helpers.addUsers(data.users, db)
+  .then(async () => {
+    for (let i = 0; i < 20; i += 1) {
+      // Insert 5 unique products 20 times
+      await helpers.addProducts(data.products, db);
+    }
   })
-}
+  .then(() => {
+      console.log('end');
+      db.connection.end()}
+    )
+  .catch(() => {
+    console.log('err');
+    db.conneciton.end();
+  });
 
-addUsers().then( async () => {
-  for (let i = 0; i < 20; i += 1) {
-    // Insert 5 unique products 20 times
-    await addProducts()
-  }
-}).then(() => connection.end())
 
 
 
-  {play_experience: 4, difficulty: 4, value: 5, build_time: 5},
 // Initially created random user data with age 1 and used randomAge to randomize age
 // Update the initial data into a data file
 // helpers.randomAge(data.users);
