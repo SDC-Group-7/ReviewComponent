5345ed4d0025fd47d392e22d2288a35487e47871
Shoji <stakashima21@gmail.com>
2020-05-03 13:32:50 -0700

    refactor(models): refactored to use promises instead of callbacks

--- models/products.js
+++ models/products.js
@@ -1,13 +1,14 @@
 const db = require('../db');
 
-const getProduct = function (id, callback) {
-  const queryString = 'SELECT * from products where id = ?';
-  db.connection.query(queryString, [id], (err, data) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, data);
-    }
+const getProduct = function (id) {
+  return new Promise((resolve, reject) => {
+    const queryString = `SELECT * from products where id = ${id}`;
+    db.connection.query(queryString, null, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(data);
+    });
   });
 };
 
--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
-
 const express = require('express');
 const models = require('../models/products');
 
 const app = express();
 
 app.get('/api/products/:product_id', (req, res) => {
-  models.getProduct(req.params.product_id, (err, data) => {
-    if (err) {
+  models.getProduct(req.params.product_id)
+    .then((product) => {
+      if (product.length === 0) {
+        res.status(404).send('Product not found');
+      } else {
+        res.status(200).json(product[0]);
+      }
+    })
+    .catch(err => {
       res.status(500).send(err);
-    } else if (data.length === 0) {
-      res.status(404).send('Product not found');
-    } else {
-      res.status(200).json(data[0]);
-    }
-  });
+    });
 });
 
 module.exports = app;
