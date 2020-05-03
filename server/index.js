03618faf443c4a5ee2a243980bdf0c16a0c8f4be
Shoji <stakashima21@gmail.com>
2020-05-03 11:18:34 -0700

    tests(server): Add basic test with supertest to test an api

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const models = require('../models/products.js');
+// const models = require('../models/products.js');
 const express = require('express');
 const app = express();
 const port = 8080;
@@ -13,4 +13,10 @@ app.get('/api/products/:product_id', (req, res) => {
   });
 });
 
+app.get('/', (req, res) => {
+  res.status(200).send('ok');
+});
+
 app.listen(port, () => console.log(`Server is listening at http://localhost:${port}`));
+
+module.exports = app;
---
+++ server/tests/index.test.js
@@ -0,0 +1,15 @@
+import "regenerator-runtime/runtime";
+import request from 'supertest';
+import server from '../';
+
+test ('api', async () => {
+  const res = await request(server).get('/');
+  expect(res.statusCode).toEqual(200);
+});
+
+describe('api', () => {
+  it('should not work', async () => {
+    const res = await request(server).get('/');
+    expect(res.statusCode).toEqual(200);
+  })
+});
