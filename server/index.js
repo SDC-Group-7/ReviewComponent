19cc684f67ed363ea8c196400a729d24dde5d176
Shoji <stakashima21@gmail.com>
2020-05-03 13:20:28 -0700

    refactor(server): Moved the server into its own file

--- server/index.js
+++ server/index.js
@@ -1,21 +1,4 @@
-// const models = require('../models/products.js');
-const express = require('express');
-const models = require('../models/products');
-const app = express();
+const app = require('./server');
 const port = 8080;
 
-app.get('/api/products/:product_id', (req, res) => {
-  models.getProduct(req.params.product_id, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else if (data.length === 0) {
-      res.status(404).send('Product not found');
-    } else {
-      res.status(200).json(data);
-    }
-  });
-});
-
 app.listen(port, () => console.log(`Server is listening at http://localhost:${port}`));
-
-module.exports = app;
