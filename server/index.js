ac122762f42d8882f4af530d39062e5cafcf29f9
Shoji <stakashima21@gmail.com>
2020-05-03 12:55:02 -0700

    feat(server): Add a 404 flow to fetch products

--- server/index.js
+++ server/index.js
@@ -8,14 +8,12 @@ app.get('/api/products/:product_id', (req, res) => {
   models.getProduct(req.params.product_id, (err, data) => {
     if (err) {
       res.status(500).send(err);
+    } else if (data.length === 0) {
+      res.status(404).send('Product not found');
     } else {
       res.status(200).json(data);
     }
   });
-});
-
-app.get('/', (req, res) => {
-  res.status(200).send('ok');
 });
 
 app.listen(port, () => console.log(`Server is listening at http://localhost:${port}`));
--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -2,14 +2,15 @@ import "regenerator-runtime/runtime";
 import request from 'supertest';
 import server from '../';
 
-test ('api', async () => {
-  const res = await request(server).get('/');
-  expect(res.statusCode).toEqual(200);
-});
-
-describe('api', () => {
-  it('should not work', async () => {
+describe('tests products api', () => {
+  it('should get a product', async () => {
     const res = await request(server).get('/api/products/1');
     expect(res.statusCode).toEqual(200);
+    expect(res.body[0]).toEqual({'id': 1, 'name': 'batman'});
+  });
+
+  it('should 404 if the id does not exist', async () => {
+    const res = await request(server).get('/api/products/101');
+    expect(res.statusCode).toEqual(404);
   })
 });
