07db60d27987ab8c3d5be9397b2ae2cfc1560c3b
Shoji <stakashima21@gmail.com>
2020-05-04 10:59:10 -0700

    tests(server): Test api getReview

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -3,20 +3,21 @@ import supertest from 'supertest';
 
 import db from '../../db';
 import server from '../server';
+import fixture from '../../legos/fixtures';
 
 const request = supertest(server);
 
-describe('tests products api', () => {
+describe('tests api', () => {
   afterAll(async(done) => {
     db.connection.end();
     done();
   });
 
+  // Test product apis
   it('should get a product', async (done) => {
-    const product = {'id': 1, 'name': 'batman'};
-    const res = await request.get(`/api/products/${product.id}`);
+    const res = await request.get(`/api/products/${fixture.product.id}`);
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual(product);
+    expect(res.body).toEqual(fixture.product);
     done();
   });
 
@@ -25,4 +26,19 @@ describe('tests products api', () => {
     expect(res.statusCode).toEqual(404);
     done();
   })
+
+  // Test review apis
+  it('should get a review', async (done) => {
+    const res = await request.get(`/api/products/${fixture.product.id}/reviews`);
+    const review = res.body[0];
+    expect(res.statusCode).toEqual(200);
+    expect(review.product_id).toEqual(fixture.product.id);
+    done();
+  });
+
+  it('should 404 if the id does not exist', async (done) => {
+    const res = await request.get(`/api/products/0/reviews`);
+    expect(res.statusCode).toEqual(404);
+    done();
+  });
 });
