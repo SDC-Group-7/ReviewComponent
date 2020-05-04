578af8c6d73d848e15680455d5d9f95201acd8fe
Shoji <stakashima21@gmail.com>
2020-05-04 11:58:07 -0700

    refactor(controllers): query does not require null because there are no placeholders in the query

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,7 +3,7 @@ const db = require('../db');
 const getReviewsForProduct = function (productId) {
   return new Promise((resolve, reject) => {
     const queryString = `SELECT * FROM reviews LEFT JOIN products ON reviews.product_id=products.id WHERE products.id=${productId}`;
-    db.connection.query(queryString, null, (err, reviews) => {
+    db.connection.query(queryString, (err, reviews) => {
       if (err) {
         console.error(err);
         return reject(err);
