ca259b55664c5c8dea3a84a9acc4dd59083ce7df
Shoji <stakashima21@gmail.com>
2020-05-05 00:55:35 -0700

    fix(client): Removing ratings component, not yet implemented

--- client/src/components/ReviewsList/ReviewExperience.jsx
+++ client/src/components/ReviewsList/ReviewExperience.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-import Rating from '../../shared/Rating';
+// import Rating from '../../shared/Rating';
 
 const ReviewExperience = () => (
   <div className="review-experience">
     <div className="review-experience__play-experience">
       <label>Play Experience</label>
-      <Rating />
+      {/* <Rating /> */}
     </div>
     <div className="review-experience__difficulty">
       <label>Level of Difficulty</label>
-      <Rating />
+      {/* <Rating /> */}
     </div>
     <div className="review-experience__value">
       <label>Value for Money</label>
-      <Rating />
+      {/* <Rating /> */}
     </div>
     <span className="review-experience__build-time">
       <span>Build Time: 8 hrs</span>
