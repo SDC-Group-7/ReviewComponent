d6ab21c5a23318d5c417d52711814ee58392d51f
Shoji <stakashima21@gmail.com>
2020-05-05 23:40:25 -0700

    fix pr comments

--- client/src/components/ReviewsOverall/index.jsx
+++ client/src/components/ReviewsOverall/index.jsx
@@ -10,7 +10,7 @@ const ReviewsOverall = () => (
     </button>
     <div className="reviews-overall__disclaimer">
       <span>
-        <p>Please note that by submitting a helpfulness vote on a review your IP address is collected and stored by our trusted third party service provider for the sole purpose of preventing multiple entries form the same IP address. To see how to controll your personal data, please see our Privacy poly.</p>
+        <p>Please note that by submitting a helpfulness vote on a review your IP address is collected and stored by our trusted third party service provider for the sole purpose of preventing multiple entries form the same IP address. To see how to controll your personal data, please see our Privacy policy.</p>
       </span>
     </div>
   </div>
