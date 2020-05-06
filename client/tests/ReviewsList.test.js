a599faa8930701b94d50c7caef21ac1389779b14
Shoji <stakashima21@gmail.com>
2020-05-06 15:45:46 -0700

    tests(ReviewList): Component requires a list of reviews

--- client/tests/ReviewsList.test.js
+++ client/tests/ReviewsList.test.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ReviewsList from '../src/components/reviewsList/index.jsx';
 
-const wrapper = shallow(<ReviewsList />);
+const reviews =
+[{
+  id: 1,
+  rating: 4,
+  recommended: { type: 'Buffer', data: [ 1 ] },
+  subject: 'lorum ipsum 3',
+  isHelpful: 0,
+  isNotHelpful: 9,
+  experience:
+   { playExperience: 4, difficulty: 2, value: 4, buildTime: 18 },
+  user: { name: 'Lonna', age: 104 }
+}];
+
+const wrapper = shallow(<ReviewsList reviews={reviews}/>);
 
 describe('Reviews List', () => {
   it('should correctly render', () => {
