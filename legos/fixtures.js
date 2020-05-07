a257563bdee2ad3e2d71f58e94b1c08d73b05d96
Shoji <stakashima21@gmail.com>
2020-05-06 17:18:25 -0700

    feat(fixture): Add another review to the fixture

--- legos/fixtures.js
+++ legos/fixtures.js
@@ -1,8 +1,19 @@
 const fixture = {
   product: { id: 1, name: 'batman' },
-  review: {
+  reviews: [{
     id: 1, rating: 5, recommended: 1, subject: 'lorum ipsum 1', isHelpful: 5, isNotHelpful: 1, productId: 1, experienceId: 1, userId: 1,
   },
+  {
+    id: 2,
+    rating: 4,
+    recommended: { type: 'Buffer', data: [ 1 ] },
+    subject: 'lorum ipsum 3',
+    isHelpful: 0,
+    isNotHelpful: 9,
+    experience:
+     { playExperience: 4, difficulty: 2, value: 4, buildTime: 18 },
+    user: { name: 'Lonna', age: 104 }
+  }],
   experience: {
     id: 1, playExperience: 4, difficulty: 4, value: 5, buildTime: 5,
   },
