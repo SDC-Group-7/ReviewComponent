14f129ea532ce585c2aedd5a540abb6051e9a0de
Shoji <stakashima21@gmail.com>
2020-05-06 17:37:57 -0700

    tests(client ReviewsList): Tests dynamically rendered review count

--- client/tests/ReviewsList.test.js
+++ client/tests/ReviewsList.test.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import fixture from '../../legos/fixtures';
 import ReviewsList from '../src/components/reviewsList/index.jsx';
 
-const reviews =
-[{
-  id: 1,
-  rating: 4,
-  recommended: { type: 'Buffer', data: [ 1 ] },
-  subject: 'lorum ipsum 3',
-  isHelpful: 0,
-  isNotHelpful: 9,
-  experience:
-   { playExperience: 4, difficulty: 2, value: 4, buildTime: 18 },
-  user: { name: 'Lonna', age: 104 }
-}];
+const wrapper = shallow(<ReviewsList reviews={fixture.reviews}/>);
 
-const wrapper = shallow(<ReviewsList reviews={reviews}/>);
-
-describe('Reviews List', () => {
+describe('Tests ReviewsList', () => {
   it('should correctly render', () => {
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it('should have the correct amount of reviews', () => {
+    const expectedReviewCount = fixture.reviews.length;
+    expect(wrapper.find('ReviewsListItem').length).toBe(expectedReviewCount);
   });
 });
 
