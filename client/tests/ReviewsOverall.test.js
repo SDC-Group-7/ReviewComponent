b22dbd042b5c41099c586b1a1b688e9a206cabf9
Shoji <stakashima21@gmail.com>
2020-05-07 16:31:39 -0700

    tests(client): Changed shallow rendering to mount, so that I can get props

--- client/tests/ReviewsOverall.test.js
+++ client/tests/ReviewsOverall.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import fixture from '../../legos/fixtures';
 import ReviewsOverall from '../src/components/ReviewsOverall';
 import ReviewsStatistics from '../src/components/ReviewsOverall/ReviewsStatistics';
@@ -7,21 +7,30 @@ import OverallExperience from '../src/components/ReviewsOverall/OverallExperienc
 
 
 describe('Test ReviewsOverall', () => {
-  const overallWrapper = shallow(<ReviewsOverall />);
+  const overallWrapper = mount(<ReviewsOverall />);
   it('should render', () => {
     expect(overallWrapper.exists('.reviews-overall')).toBe(true);
   });
 });
 
 describe('Test ReviewsStatistics', () => {
-  const statisticsWrapper = shallow(<ReviewsStatistics />);
+  const statisticsWrapper = mount(<ReviewsStatistics reviews={fixture.reviews}/>);
   it('should render', () => {
     expect(statisticsWrapper.exists('.reviews-statistics')).toBe(true);
   });
+
+  it('should have the reviews props', () => {
+    console.log(statisticsWrapper);
+    expect(statisticsWrapper.prop('reviews')).toEqual(fixture.reviews);
+  });
+
+  // I need to write a test
+  // this test will check that the aggregated data has all the right values
+
 });
 
 describe('Test OverallExperience', () => {
-  const overallExperienceWrapper = shallow(<OverallExperience />);
+  const overallExperienceWrapper = mount(<OverallExperience />);
   it('should render', () => {
     expect(overallExperienceWrapper.exists('.overall-experience')).toBe(true);
   });
