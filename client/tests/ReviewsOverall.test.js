5e30c0ddb0abca27576e2d37c1067297cd2e401c
Shoji <stakashima21@gmail.com>
2020-05-07 21:22:12 -0700

    tests(client): Initial base

--- client/tests/ReviewsOverall.test.js
+++ client/tests/ReviewsOverall.test.js
@@ -20,8 +20,23 @@ describe('Test ReviewsStatistics', () => {
   });
 
   it('should have the reviews props', () => {
-    console.log(statisticsWrapper);
     expect(statisticsWrapper.prop('reviews')).toEqual(fixture.reviews);
+  });
+
+  it('should have aggregated ratings', () => {
+    // this should test that the ratings have been aggrevated
+    // it should do this by getting all of the reviews and dividing them by the number of reviews to get the average
+
+    const totalRatings = fixture.reviews.reduce((total, review) => {
+      return total + review.rating;
+    }, 0);
+
+    const averageRatings = totalRatings / fixture.reviews.length;
+
+    // once i get the reviews, I want to mount the ocmponent
+    // check the overall rating and match it
+
+    expect(statisticsWrapper.find('reviews-statistics__title').contains(totalRatings)).toBe(true);
   });
 
   // I need to write a test
