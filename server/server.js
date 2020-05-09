41b18bf1555fead9eaf92506e97acfea491f6216
stak21 <stakashima21@gmail.com>
2020-05-09 15:57:21 -0700

    feat(server): Add route to update reviews

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Reviews = require('../controllers/reviews');
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   console.log(`Incoming ${req.method} request to ${req.path}`);
@@ -19,6 +22,19 @@ app.get('/api/products/:product_id/reviews', (req, res) => {
       } else {
         res.status(200).json(reviews);
       }
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    });
+});
+
+app.put('/api/products/:product_id/reviews/:review_id', (req, res) => {
+  const { product_id: productId, review_id: reviewId } = req.params;
+  const data = req.body;
+
+  Reviews.updateReviewForProduct(productId, reviewId, data)
+    .then((result) => {
+      res.json(result);
     })
     .catch((err) => {
       res.status(404).send(err);
