f93a234db5a33470f4574afca702cca19763f936
stak21 <stakashima21@gmail.com>
2020-05-08 23:34:06 -0700

    feat(clients): Use svg for the thumbsup and down

--- client/src/components/ReviewsList/ReviewHelpfulness.jsx
+++ client/src/components/ReviewsList/ReviewHelpfulness.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import thumbsup from '../../assets/svg/thumbsup.svg';
+import thumbsdown from '../../assets/svg/thumbsdown.svg';
+
 const ReviewHelpfulness = () => (
   <div className="review__helpfulness">
     <h5 className="title">Was this helpful?</h5>
     <HelpfulnessCountContainer >
-      <ThumbButton >helpful 10</ThumbButton>
-      <ThumbButton >not helpful 1</ThumbButton>
+      <ThumbButton >
+        <img src={thumbsup}></img>
+        <Text>1</Text>
+      </ThumbButton>
+      <ThumbButton >
+        <img src={thumbsdown}></img>
+        <Text>0</Text>
+      </ThumbButton>
     </HelpfulnessCountContainer>
   </div>
 );
@@ -20,4 +29,11 @@ const HelpfulnessCountContainer = styled.div`
 
 const ThumbButton = styled.button`
   margin: 1rem;
+`;
+
+const Text = styled.span`
+  color: rgb(44, 44, 44);
+  font-size: 0.75rem;
+  line-height: 1.1875rem;
+  font-weight: 500;
 `;
