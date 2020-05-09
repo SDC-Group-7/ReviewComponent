d7f50abd1965454de4776609ff2690845db8a57e
stak21 <stakashima21@gmail.com>
2020-05-08 21:13:34 -0700

    refactor(client): Remove unused imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './components/ReviewsList/ReviewsList.css';
-import './components/ReviewsOverall/ReviewsOverall.css';
 import './styles.css';
 import ReviewsOverview from './components/ReviewsOverview';
 
