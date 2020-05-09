95309e24d68b1379f708ddceb0d98120766035d8
stak21 <stakashima21@gmail.com>
2020-05-09 15:22:53 -0700

    build(*): Setup environment to use jest fetch mocks

--- enzyme.config.js
+++ enzyme.config.js
@@ -1,5 +1,5 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 require('isomorphic-fetch');
-
+require('jest-fetch-mock').enableMocks();
 configure({ adapter: new Adapter() });
