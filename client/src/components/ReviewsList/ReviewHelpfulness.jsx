b67e670e8e428147a6bc4059d2bae1e31c9812fb
stak21 <stakashima21@gmail.com>
2020-05-08 23:50:27 -0700

    feat(client): Add styling to helpfullness

--- client/src/components/ReviewsList/ReviewHelpfulness.jsx
+++ client/src/components/ReviewsList/ReviewHelpfulness.jsx
@@ -6,14 +6,20 @@ import thumbsdown from '../../assets/svg/thumbsdown.svg';
 
 const ReviewHelpfulness = () => (
   <div className="review__helpfulness">
-    <h5 className="title">Was this helpful?</h5>
+    <TSpacer>
+      <Title>
+        Was this helpful?
+      </Title>
+    </TSpacer>
     <HelpfulnessCountContainer >
       <ThumbButton >
         <img src={thumbsup}></img>
+        <LSpacer></LSpacer>
         <Text>1</Text>
       </ThumbButton>
       <ThumbButton >
         <img src={thumbsdown}></img>
+        <LSpacer></LSpacer>
         <Text>0</Text>
       </ThumbButton>
     </HelpfulnessCountContainer>
@@ -25,15 +31,43 @@ export default ReviewHelpfulness;
 
 const HelpfulnessCountContainer = styled.div`
   display: flex;
+  padding-bottom: 1.125rem;
+  align-items: center;
 `;
 
 const ThumbButton = styled.button`
   margin: 1rem;
+  display: flex;
+  align-items: center;
+  background: transparent;
+  border-width: 0px;
+  cursor: pointer;
 `;
+
+const Title = styled.span`
+  font-size: 1rem;
+  line-height: 1.5;
+  font-weight: 400;
+`;
+
 
 const Text = styled.span`
   color: rgb(44, 44, 44);
   font-size: 0.75rem;
   line-height: 1.1875rem;
   font-weight: 500;
+`;
+
+const LSpacer = styled.div`
+  padding-top: 0px;
+  padding-right: 0px;
+  padding-bottom: 0px;
+  padding-left: 0.75rem;
+  `;
+
+const TSpacer = styled.div`
+  padding-top: 0.75px;
+  padding-right: 0px;
+  padding-bottom: 0px;
+  padding-left: 0rem;
 `;
