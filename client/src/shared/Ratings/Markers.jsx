50901aa4d077df0adb19fbbfb55c0329b6b08485
stak21 <stakashima21@gmail.com>
2020-05-10 03:51:01 -0700

    feat(client): Implement Ratings with star svg and styling

--- client/src/shared/Ratings/Markers.jsx
+++ client/src/shared/Ratings/Markers.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 
+import fullStars from '../../assets/svg/fullStars.svg';
+import thumbsup from '../../assets/svg/thumbsup.svg';
 const Markers = () => (
-  // TODO
-  <div>Marker</div>
+  <MarkerLayout>
+    <StarContainer>
+      <img src={fullStars} />
+    </StarContainer>
+    <StarContainer>
+      <img src={fullStars} />
+    </StarContainer>
+    <StarContainer>
+      <img src={fullStars} />
+    </StarContainer>
+    <StarContainer>
+      <img src={fullStars} />
+    </StarContainer>
+    <StarContainer>
+      <img src={fullStars} />
+    </StarContainer>
+  </MarkerLayout>
 );
 
 export default Markers;
+
+const StarContainer = styled.div`
+  width: 1.5625rem;
+  height: 1.5625rem;
+  margin-right: 0.38rem;
+`;
+
+const MarkerLayout = styled.div`
+  display: flex;
+  align-items: center;
+`;
