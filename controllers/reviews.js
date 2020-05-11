b9cb8dac6b4b0675223e5ef77604778667ef6786
stak21 <stakashima21@gmail.com>
2020-05-10 21:11:00 -0700

    feat(controllers): Update controllers to include description

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -4,7 +4,7 @@ const helpers = require('./helpers');
 const getReviewsForProduct = function (productId) {
   return new Promise((resolve, reject) => {
     const queryString = `
-    SELECT prod.name AS product_name, prod.id AS product_id, rev.id, rev.created_at, rev.rating, rev.recommended, rev.subject, rev.is_helpful, rev.is_not_helpful, exp.play_experience, exp.difficulty, exp.value, exp.build_time, users.name, users.age FROM products AS prod
+    SELECT prod.name AS product_name, prod.id AS product_id, rev.id, rev.created_at, rev.rating, rev.recommended, rev.subject, rev.description, rev.is_helpful, rev.is_not_helpful, exp.play_experience, exp.difficulty, exp.value, exp.build_time, users.name, users.age FROM products AS prod
     JOIN reviews AS rev ON rev.product_id=prod.id
     JOIN experiences AS exp ON exp.id=rev.experience_id
     JOIN users ON users.id=rev.user_id
