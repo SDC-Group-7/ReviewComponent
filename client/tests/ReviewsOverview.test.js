42da7f3e96ee3b2b2e90d2e53bfd2319adcd4968
stak21 <stakashima21@gmail.com>
2020-05-10 17:44:19 -0700

    tests(client): Fix ReviewOverview test, which now tests when there are no reviews, a write a review button renders

--- client/tests/ReviewsOverview.test.js
+++ client/tests/ReviewsOverview.test.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import fixture from '../../legos/fixtures';
 import ReviewsOverview from '../src/components/ReviewsOverview/';
-
+import { Button } from '../src/shared/StyledComponents';
 
 describe('Tests ReviewsOverview', () => {
-  const reviewsOverviewWrapper = shallow(<ReviewsOverview />);
-  it('should render', () => {
-    expect(reviewsOverviewWrapper.exists('.reviews-overview')).toBe(true);
+  const reviewsOverviewWrapper = shallow(<ReviewsOverview reviews={[]}/>);
+
+  it('should not render reviews, but render a write a review button', () => {
+    expect(reviewsOverviewWrapper.find(Button).prop('children')).toEqual('Write a review');
   });
 });
