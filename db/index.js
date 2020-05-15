fd9b77ba9e57d852af2b40e12e12a53a4918c146
stak21 <stakashima21@gmail.com>
2020-05-15 12:49:10 -0700

    build(docker): Update host to use docker container name

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 const connection = mysql.createPool({
   connectionLimit: 100,
-  host: process.env.HOST,
+  host: 'mysql',
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB,
