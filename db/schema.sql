c6c0435f3b7940dc1d56456d2fc0ac55a48e69f1
Andrew Cadwallader <andrew.cadwallader@gmail.com>
2020-05-20 21:57:06 -0400

    Revise schema.sql per feedback

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,27 @@ CREATE DATABASE legos;
 USE legos;
 
 CREATE TABLE users (
-	id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(50) NOT NULL,
-  experience TINYINT UNSIGNED NOT NULL 
+  id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(32) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  building_experience BIT(2) UNSIGNED,
 );
 
 CREATE TABLE reviews (
-	id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	user_id BIGINT UNSIGNED NOT NULL,
-	product_id BIGINT UNSIGNED NOT NULL,
-	created_at DATE NOT NULL DEFAULT CURRENT_DATE,
-	rating BIT(3) NOT NULL,
-	recommended BOOLEAN NOT NULL,
-	subject VARCHAR(50) NOT NULL,
-	description VARCHAR(1550) NOT NULL,
-	is_helpful BIGINT UNSIGNED DEFAULT 0,
-	is_not_helpful BIGINT UNSIGNED DEFAULT 0,
-	play_experience BIT(3) NOT NULL,
-	difficulty BIT(3) NOT NULL,
-	value BIT(3) NOT NULL,
-	build_time TINYINT UNSIGNED NOT NULL,
+  id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user_id BIGINT UNSIGNED NOT NULL,
+  product_id BIGINT UNSIGNED NOT NULL,
+  created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+  rating BIT(3) NOT NULL,
+  would_recommend BOOLEAN NOT NULL,
+  purchased_for BIT(2),
+  headline VARCHAR(32) NOT NULL,
+  review VARCHAR(255) NOT NULL,
+  is_helpful TINYINT UNSIGNED DEFAULT 0,
+  is_not_helpful TINYINT UNSIGNED DEFAULT 0,
+  play_experience BIT(3),
+  level_of_difficulty BIT(3),
+  value_for_money BIT(3),
+  build_time TINYINT UNSIGNED,
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
