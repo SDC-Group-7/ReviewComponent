55037d7369ec9e5636cb4b1e29a8a72be7a2ebac
Andrew Cadwallader <andrew.cadwallader@gmail.com>
2020-06-02 15:46:12 -0400

    Make longer reviews

--- db/seed/server.py
+++ db/seed/server.py
@@ -1,4 +1,4 @@
-from collections import Counter,defaultdict
+from collections import Counter, defaultdict
 import random
 import gzip
 import textwrap
@@ -10,7 +10,7 @@ def make_trigrams(filename):
     startwords = [bigram]
     for w in words[2:] + words[:2]:
         trigrams[bigram].append(w)
-        if bigram[0].endswith('.') and bigram[1][0].isupper():
+        if (bigram[0].endswith('.') or bigram[0].endswith('!') or bigram[0].endswith('?')) and bigram[1][0].isupper():
             startwords.append((bigram[1], w))
         bigram = (bigram[1], w)
     return trigrams,startwords
@@ -18,14 +18,14 @@ def make_trigrams(filename):
 def random_text(trigrams, startwords, num_words = 10):
     current_pair = random.choice(startwords)
     random_text = list(current_pair)
-    while len(random_text) < num_words or not random_text[-1].endswith('.'):
+    while len(random_text) < num_words or not (random_text[-1].endswith('.') or random_text[-1].endswith('!') or random_text[-1].endswith('?')):
         next = random.choice(trigrams[current_pair])
         random_text.append(next)
         current_pair = (current_pair[1], next)
-        if len(random_text) > 2 * num_words: random_text[-1] += '.'
-    return textwrap.fill(' '.join(random_text), width=140)
+        if len(random_text) > 4 * num_words: random_text[-1] += '...'
+    return textwrap.fill(' '.join(random_text), width = 140)
 
-tri_lego, start_lego = make_trigrams('lego.reviews.txt')
+tri_lego, start_lego = make_trigrams('./db/seed/lego.reviews.txt')
 
 while True:
     try:
