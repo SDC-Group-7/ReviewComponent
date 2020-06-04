7dfa5d32adef69c5ad96d11984dd606db79d72da
Andrew Cadwallader <andrew.cadwallader@gmail.com>
2020-06-04 12:19:18 -0400

    Specify python3

--- db/seed/seed.sh
+++ db/seed/seed.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mkfifo fin
 mkfifo fout
-python db/seed/server.py < fout > fin &
+python3 db/seed/server.py < fout > fin &
 node db/seed/seed.js 2>&1 > fout < fin $1 && rm fin && rm fout
